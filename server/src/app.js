fcdf1c59ba0722d9af5d0639fecf5764f28e29c8
Benjamin Chipinde <benjamin@kylix.online>
2022-06-14 01:36:13 +0300

    npm automation

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
+
+const path = require('path')
 
 const planetRouter = require('./routes/planets/planets.router')
 
@@ -8,6 +11,16 @@ const app = express()
 app.use(cors({
   origine: 'http://localhost:5000'
 }))
+
+app.use(morgan('combined'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.use(planetRouter)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = app
